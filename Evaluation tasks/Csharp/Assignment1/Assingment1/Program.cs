fafbad63c82c24df227428706b6e4dfaec443ce6
Vidushi Jain <vidushij@infinite.com>
2025-06-18 18:29:34 +0530

    Added assignment 1

---
+++ Evaluation tasks/Csharp/Assignment1/Assingment1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Assingment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            pg.comparison_Num();
+            Console.ReadLine();
+            Program1.Positivity_Check();
+           
+        }
+
+        public void comparison_Num()
+        {
+            Console.WriteLine("Ques : Check if the numbers are equal or not");
+            Console.WriteLine("Enter 1st number");
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int b;
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a == b)
+                Console.WriteLine("{0} and {1} are equal", a, b);
+            else
+                Console.WriteLine("{0} and {1} are not equal", a, b);
+        }
+    }
+}
---
+++ Evaluation tasks/Csharp/Assignment1/Assingment1/Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Assingment1
+{
+    internal class Program1
+    {
+        public static void Positivity_Check()
+        {
+            Console.WriteLine("Ques : Checking the positivity of a number");
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0)
+                Console.WriteLine("{0} is a positive number", num);
+            else
+                Console.WriteLine("{0} is a negative number", num);
+            Console.ReadLine();
+
+        }
+    }
+}
